0311f2c67027dcbcc9a7213fd6d359167351d4da
DStu <DiscoStu.BSW@gmail.com>
2012-11-15 20:06:55 +0100

    That is not needed anymore

--- apostrophe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# replaces 'wrong' spellings of KC, PS, WV, FG with 'correct' apostrophes
-# also removes additional spaces in internal links
-#
-python scripts/replace.py -start:! -pt:3 -summary:"Correcting apostrophes, internal links" -regex -replacementfile:"apostrophe.txt"
