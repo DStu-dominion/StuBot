180d1a5b6d1150e41eecbd626a11d9be2599546e
DStu <DiscoStu.BSW@gmail.com>
2012-12-18 19:03:02 +0100

    Handling of apostophes for card template

--- generateCardTemplate.sh
+++ generateCardTemplate.sh
@@ -2,4 +2,6 @@
 #
 # Generates the regexes to replace internal links with cardlists from the list of cards in cardlist.txt
 #
-less cardlist.txt | sed -r 's/(.*)/\(\[^a-zA-Z\]\)\\\[\\\[\1\\\]\\\]\(\[^a-zA-Z\]\)\n\\1{\{Card\|\1\}\}\\2\n\(\[a-zA-Z\]\*\)\\\[\\\[\1\\\]\\\]\(\[a-zA-Z\]\*\)\n\{\{Card\|\1\|\\1\1\\2}}\n\\\[\\\[\1\\\|\(\[^\\\]\]\*\)\\\]\\\]\n\{\{Card\|\1\|\\1\}\}/g' > out.txt
+# s/’/’'\`/g is a workaround because handeling apostrophs seems to be way more difficult with sed -r than with sed
+#
+less cardlist.txt | sed -r 's/(.*)/\(\[^a-zA-Z\]\)\\\[\\\[\1\\\]\\\]\(\[^a-zA-Z’\]\)\n\\1{\{Card\|\1\}\}\\2\n\(\[a-zA-Z\]\*\)\\\[\\\[\1\\\]\\\]\(\[a-zA-Z’\]\*\)\n\{\{Card\|\1\|\\1\1\\2}}\n\\\[\\\[\1\\\|\(\[^\\\]\]\*\)\\\]\\\]\n\{\{Card\|\1\|\\1\}\}/g' | sed "s/’/’'\`/g" > out.txt
