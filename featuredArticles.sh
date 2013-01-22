5ef6c1fdf0bbbff1060173c758a13fccd1db8f39
DStu <DiscoStu.BSW@gmail.com>
2013-01-22 21:13:12 +0100

    Let the bot track the featured articles

---
+++ featuredArticles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#
+
+# get featured article
+FA=$(GET "http://wiki.dominionstrategy.com/api.php?action=query&prop=revisions&rvprop=content&titles=Template:Featured_article")
+LINK=$(echo $FA | grep -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])' | grep -m 1 -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])')
+
+
+#Want english names for the month
+LC_TIME=en_US.UTF-8
+YEAR=$(date +%Y)
+MONTH=$(date +%B)
+PAGEARCHIVE="User:DStu/OldFeaturedArticles"
+
+# todo: once a year insert new year
+HEADING="Former Featured Articles"
+
+#Add new year if current year is not in the list yet
+python scripts/replace.py -page:$PAGEARCHIVE -excepttext:"=== $YEAR ===" -regex "(== $HEADING ==)" "\1\n=== $YEAR ==="
+
+#Add link to featured article if it's not in (this years) list
+#Todo: Don't include, if it was last on last years list?
+python scripts/replace.py -page:$PAGEARCHIVE -excepttext:"=== ($YEAR)|(($YEAR-1)) ===\n[^\n]*$LINK" -regex "(=== $YEAR ===)" "\1\n* $MONTH: $LINK"
+
