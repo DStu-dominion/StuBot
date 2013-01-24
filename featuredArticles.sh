669d3f3b7ad59e89a905329ded1224cbd4bf154a
DStu <DiscoStu.BSW@gmail.com>
2013-01-24 19:40:04 +0100

    DYK and random stuff

--- featuredArticles.sh
+++ featuredArticles.sh
@@ -2,23 +2,32 @@
 #
 #
 
-# get featured article
+# get featured article/DYK
 FA=$(GET "http://wiki.dominionstrategy.com/api.php?action=query&prop=revisions&rvprop=content&titles=Template:Featured_article")
-LINK=$(echo $FA | grep -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])' | grep -m 1 -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])')
-
+DYK=$(GET "http://wiki.dominionstrategy.com/index.php?action=raw&title=Template:Did_you_know")
+FALINK=$(echo $FA | grep -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])' | grep -m 1 -oE '(\{\{[^\}]*\}\})|(\[\[[a-zA-Z\|]*\]\])')
 
 #Want english names for the month
-LC_TIME=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
 YEAR=$(date +%Y)
 MONTH=$(date +%B)
 PAGEARCHIVE="User:DStu/OldFeaturedArticles"
+PAGEDYK="User:DStu/OldDidYouKnow"
 
-# todo: once a year insert new year
-HEADING="Former Featured Articles"
+DYKHEADING="Former Did You Know"
+FAHEADING="Former Featured Articles"
+
+# Featured Articles:
 
 #Add new year if current year is not in the list yet
-python scripts/replace.py -page:$PAGEARCHIVE -excepttext:"=== $YEAR ===" -regex "(== $HEADING ==)" "\1\n=== $YEAR ===" -summary:"Happy new year"
+python scripts/replace.py -page:$PAGEARCHIVE -excepttext:"=== $YEAR ===" -regex "(== $FAHEADING ==)" "\1\n=== $YEAR ===" -summary:"Happy new year $YEAR" -always
+#Add link to featured article if it's not in the list
+python scripts/replace.py -page:$PAGEARCHIVE -regex "(=== $YEAR ===)" "\1\n* $MONTH: $FALINK" -excepttext:"=== (($YEAR)|(($YEAR-1))) ===\n[^\n]*$FALINK" -summary:"Archived old featured articles" -always
 
-#Add link to featured article if it's not in (this years) list
-python scripts/replace.py -page:$PAGEARCHIVE -excepttext:"=== ($YEAR)|(($YEAR-1)) ===\n[^\n]*$LINK" -regex "(=== $YEAR ===)" "\1\n* $MONTH: $LINK" -summary:"Archived old featured articles"
+
+# DIY:
+#Add new year if current year is not in the list yet
+python scripts/replace.py -page:$PAGEDYK -excepttext:"=== $YEAR ===" -regex "(== $DYKHEADING ==)" "\1\n=== $YEAR ===" -summary:"Happy new year $YEAR" -always
+#Add text to former DYK if it's not in the list
+python scripts/replace.py -page:$PAGEDYK -excepttext:"$DYK" -regex "(=== $YEAR ===)" "\1\n* $MONTH:\n$DYK" -summary:"Archived old DYK" -always
 
